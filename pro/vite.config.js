11bdda8c18c62d69b421a9891a6e6ba965812214
Vivekscodes <viveks2026@mpgi.edu.in>
2025-05-21 22:59:58 +0530

    Minor Code changes done

--- pro/vite.config.js
+++ pro/vite.config.js
@@ -1,7 +1,12 @@
+```javascript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // React plugin for Vite
+    react()
+  ],
 })
+```
