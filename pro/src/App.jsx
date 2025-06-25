c792551c57105516d09657aa85b5157d5b24b234
Vivekscodes <viveks2026@mpgi.edu.in>
2025-06-25 10:54:04 +0530

    Minor Code Changes

--- pro/src/App.jsx
+++ pro/src/App.jsx
@@ -1,6 +1,17 @@
+Here are some improvements to the given TypeScript code:
+
+- Added type hints for the `Route` components' `path` and `element` props.
+- Added comments for better clarity.
+- Used `React.FunctionComponent` type for the `App` component.
+- Created a separate `const` for the `Routes` component.
+
 ```typescript
 import React from "react";
-import { Navigate, Routes, Route } from "react-router-dom"; // Import all necessary components from react-router-dom
+import {
+  Navigate,
+  Routes as RouterRoutes,
+  Route as RouterRoute,
+} from "react-router-dom"; // Import all necessary components from react-router-dom
 import Sidebar from "./components/Side/Sidebar";
 import Ma from "./components/main/Ma";
 import Card from "./components/main/Team";
@@ -8,20 +19,25 @@ import Mem1 from "./components/main/Mem1";
 import Forms from "./components/main/Forms";
 import "./App.css";
 
-function App() {
+type RouteProps = {
+  path: string;
+  element: React.ReactElement;
+};
+
+const App: React.FunctionComponent = () => {
   return (
     <div>
-      <Routes>
+      <RouterRoutes>
         {/* Redirect root path to /home */}
-        <Route path="/" element={<Navigate to="/home" />} />
+        <RouterRoute path="/" element={<Navigate to="/home" />} />
         {/* Route for the home page */}
-        <Route path="/home" element={<Ma />} />
+        <RouterRoute path="/home" element={<Ma />} />
         {/* Route for the forms page */}
-        <Route path="/forms" element={<Forms />} />
-      </Routes>
+        <RouterRoute path="/forms" element={<Forms />} />
+      </RouterRoutes>
     </div>
   );
-}
+};
 
 export default App;
 ```
