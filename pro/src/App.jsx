7d284220592aac6f06674b2d78957c40550f01ae
Vivekscodes <viveks2026@mpgi.edu.in>
2025-06-25 10:17:35 +0530

    Minor Code Changes

--- pro/src/App.jsx
+++ pro/src/App.jsx
@@ -1,23 +1,27 @@
+```typescript
 import React from "react";
+import { Navigate, Routes, Route } from "react-router-dom"; // Import all necessary components from react-router-dom
 import Sidebar from "./components/Side/Sidebar";
 import Ma from "./components/main/Ma";
 import Card from "./components/main/Team";
 import Mem1 from "./components/main/Mem1";
-import "./App.css";
-import { Navigate, Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
 import Forms from "./components/main/Forms";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Routes>
+        {/* Redirect root path to /home */}
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path ='/home' element={<Ma/>}/>
-        <Route path="/forms" element={<Forms/>}/>
+        {/* Route for the home page */}
+        <Route path="/home" element={<Ma />} />
+        {/* Route for the forms page */}
+        <Route path="/forms" element={<Forms />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+```
